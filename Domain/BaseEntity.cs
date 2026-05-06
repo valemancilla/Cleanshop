5d8bbbe3ec9ef57331e7a62f8d2440ca8fbffe09
valemancilla <valem2958@gmail.com>
2026-05-06 08:23:24 -0500

    feat: :ambulance: .net

--- Domain/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Domain;
-
-public abstract class BaseEntity<TId>
-{
-    public TId Id { get; protected set; } = default!;
-    protected BaseEntity() { }
-}
--- Infrastructure/Context/AppDbContext.cs
+++ Infrastructure/Context/AppDbContext.cs
@@ -1,14 +1,15 @@
 using System;
+using Domain.Entities.Products;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Context;
 
 public sealed class AppDbContext : DbContext
 {
+    public DbSet<Product> Products { get; set; } = default!;
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
